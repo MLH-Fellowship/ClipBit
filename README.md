7fba15769dc8ec8c899c42e5b14f46f738c0bca8
Kimaru thagana <macbuntu@localhost.localdomain>
2021-02-04 21:38:44 +0300

    clean up code

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Generates a summary of any YouTube video and allows the user to extract meaningful information using custom keywords.
 
 # Text Summary Module
-Uses pretrained **tokenizer** and **models** from [Torch](https://pytorch.org/tutorials/beginner/deep_learning_nlp_tutorial.html) to speed up the development process.
+Uses pretrained **tokenizer** and **models** from [Torch](https://pytorch.org/get-started/previous-versions/) to speed up the development process.
 Uses the abstractive summary approach where new sentences are formed from the input text.
--- text_summary.py
+++ text_summary.py
@@ -1,6 +1,5 @@
 import torch
-import json
-from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 model = T5ForConditionalGeneration.from_pretrained('t5-small') # use pretrained model
 tokenizer = T5Tokenizer.from_pretrained('t5-small') # use pretrained tokenizer
@@ -29,17 +28,3 @@ def text_summary(text):
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
-
-text ="""
-The US has "passed the peak" on new coronavirus cases, President Donald Trump said and predicted that some states would reopen this month.
-
-The US has over 637,000 confirmed Covid-19 cases and over 30,826 deaths, the highest for any country in the world.
-
-At the daily White House coronavirus briefing on Wednesday, Trump said new guidelines to reopen the country would be announced on Thursday after he speaks to governors.
-
-"We'll be the comeback kids, all of us," he said. "We want to get our country back."
-
-The Trump administration has previously fixed May 1 as a possible date to reopen the world's largest economy, but the president said some states may be able to return to normalcy earlier than that.
-"""
-
-print(text_summary(text))
