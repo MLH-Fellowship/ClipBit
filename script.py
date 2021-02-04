87f96c5ecb8f6f03b7c881a968560a4aa2ce51f8
Miguel Guardia <guardia@ualberta.ca>
2021-02-04 15:50:19 -0700

    Create new Class that creates a captions srt file, captions text file, and is contained within a captions object

---
+++ script.py
@@ -0,0 +1,52 @@
+import os
+import re
+import pysrt
+import logging
+from pathlib import Path
+from pytube import YouTube
+
+logger = logging.getLogger(__name__)
+
+
+class Captions:
+    def __init__(self, URL):
+        self.yt = YouTube(URL)
+        self.video_path = os.getcwd() + "/videos/{}".format(
+            self.yt.title.lower().replace(" ", "_")
+        )
+        Path(self.video_path).mkdir(parents=True, exist_ok=True)
+        self.srt_filename = self.create_srt()
+        self.generate_script()
+
+    def create_srt(self):
+        try:
+            caption_dict = self.yt.captions.lang_code_index
+            en_key = None
+            for key in caption_dict.keys():
+                if re.findall("^en.*", key):
+                    en_key = key
+                    break
+
+            if en_key:
+                self.yt.captions[en_key].download("captions")
+            elif caption_dict.get("a.en", None):
+                self.yt.captions["a.en"].download("captions")
+                en_key = "a.en"
+            else:
+                logger.error("clipbit: error in downloading caption file")
+
+            os.rename(f"captions ({en_key}).srt", f"{self.video_path}/captions.srt")
+            return f"{self.video_path}/captions.srt"
+
+        except Exception as e:
+            logger.error(e)
+            logger.error("clipbit: error in downloading captions.")
+
+    def generate_script(self):
+        srt_file = pysrt.open(self.srt_filename)
+        text = ""
+        for obj in srt_file:
+            text += f"{obj.text} "
+
+        with open(f"{self.video_path}/script.txt", "w+") as f:
+            f.write(text)
