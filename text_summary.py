0ebbf4b9855010e8d0a6f9ed184a6173159c14a2
Kimaru thagana <macbuntu@localhost.localdomain>
2021-02-04 04:30:51 +0300

    function setup

---
+++ text_summary.py
@@ -0,0 +1,11 @@
+
+
+
+def text_summary(text):
+    '''
+
+    :param text: body of text to be summarized
+    :return: summarized version of original text
+    '''
+    summary = ''
+    return summary
