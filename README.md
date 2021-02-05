2543bab06981cb323ab5490891e46e5fa9be52f2
Kimaru Thagana <30570908+KimaruThagna@users.noreply.github.com>
2021-02-05 21:38:46 +0300

    Text summarization (#12)
    * add text summarization .py file
    
    * function setup
    
    * add requirements
    
    * add explanation for text summary module #2
    
    * clean up code
    
    * cleanup code base
    
    * delete read me
    
    * remove requirements.txt
    
    Co-authored-by: Kimaru thagana

--- README.md
+++
@@ -1,3 +0,0 @@
-# ClipBit
-
-Generates a summary of any YouTube video and allows the user to extract meaningful information using custom keywords.
---
+++ text_summary.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+model = T5ForConditionalGeneration.from_pretrained('t5-small') # use pretrained model
+tokenizer = T5Tokenizer.from_pretrained('t5-small') # use pretrained tokenizer
+device = torch.device('cpu')
+
+
+def text_summary(text):
+    '''
+    Abstractive text summarization
+    :param text: body of text to be summarized
+    :return: summarized version of original text
+    '''
+    # preprocessing
+    clean_text = text.strip().replace("\n","")
+    t5_text = "summarize: " + clean_text # define task
+
+    #tokenizer
+    tokenized_text = tokenizer.encode(t5_text, return_tensors="pt").to(device)
+    # summmarize
+    summary_ids = model.generate(tokenized_text,
+                                 num_beams=4,
+                                 no_repeat_ngram_size=2,
+                                 min_length=30,
+                                 max_length=100,
+                                 early_stopping=True)
+
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
