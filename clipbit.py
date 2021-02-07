ae2782599f950570bbc385806a85e93f3c78178f
Sarthak Khattar <sarthakoct@gmail.com>
2021-02-08 00:55:31 +0530

    added color coding, verbose mode, made refinements across all 3 files (#14)
    * added color coding, verbose mode, made refinements across all 3 files
    
    fixed formatting
    
    * Added docstrings, refined pretty printing using rich

--- clipbit.py
+++ clipbit.py
@@ -4,17 +4,23 @@ import click
 import os
 import sys
 import logging
+import colorama
+from rich.console import Console
+from rich.table import Table
 
 from script import Captions
+from text_summary import summary as ts
 
 logger = logging.getLogger(__name__)
+colorama.init()
+console = Console()
 
 
 @click.command()
 @click.option(
     "-s",
     "--summarize",
-    prompt="Enter the YouTube link:",
+    prompt="\033[93mEnter the YouTube link:\033[0m",
     help="Generates summary from the YouTube link.",
 )
 @click.option(
@@ -23,13 +29,44 @@ logger = logging.getLogger(__name__)
     default=False,
     help="Verbose mode: shows progress bar and other information.",
 )
-def generate_summary(summarize, verbose):
-    captions_obj = Captions(summarize)
+@click.option(
+    "-d/-c",
+    "--detailed/--clean",
+    default=False,
+    help="Detailed mode: displays title, captions and summary in a table.",
+)
+def generate_summary(summarize, verbose, detailed):
+    verboseprint = print if verbose else lambda *a, **k: None
 
-    # TO-DO
-    # captions_obj.video_path + "/script.txt" is the path to the combined script.
-    # import your file and then
-    # call your NLP model function here.
+    captions = Captions(summarize, verbose)
+    if captions.srt_filename:
+        script, final_summary = ts(captions.video_path, verbose)
+        if final_summary:
+            if detailed:
+                pp_print(script, final_summary, captions)
+            else:
+                print(f"\n{final_summary}\n")
+            verboseprint(
+                "\033[96m\u2192 Captions, Script and Summary are all available under videos/<video_name>/ directory.\033[0m"
+            )
+
+
+# Pretty prints the results
+def pp_print(script, summary, captions):
+    """
+    OPTIONAL
+    Pretty prints filename, script and summar.
+    :param summary: final generated summary from the NLP model
+    :param captions: script.Captions object
+
+    """
+    table = Table(show_header=True, header_style="bold red")
+    table.add_column("VIDEO", style="dim", width=12, justify="center")
+    table.add_column("CAPTIONS", style="dim", justify="center")
+    table.add_column("SUMMARY", style="dim", justify="center")
+    table.add_row(captions.yt.title, script, summary)
+
+    console.print(table)
 
 
 # Returns the size of the terminal
@@ -40,6 +77,14 @@ def get_terminal_size():
 
 # Displays a simple progress bar
 def display_progress_bar(bytes_received, filesize, ch="█", scale=0.55):
+    """
+    OPTIONAL
+    Creates and displays a simple progress bar.
+    :param bytes_received: bytes successfully received
+    :param filesize: total filesize in bytes
+    :param scale: scaling coefficient for the loading bar
+
+    """
     _, columns = get_terminal_size()
     max_width = int(columns * scale)
 
@@ -54,6 +99,13 @@ def display_progress_bar(bytes_received, filesize, ch="█", scale=0.55):
 
 # On download progress callback function
 def on_progress(stream, chunk, bytes_remaining):
+    """
+    callback function for pytube to use to display the bar
+    :param stream: pytube.YouTube.stream object
+    :param chunk: chuck of bytes
+    :param bytes_remaining: bytes left to download
+
+    """
     filesize = stream.filesize
     bytes_received = filesize - bytes_remaining
     display_progress_bar(bytes_received, filesize)
@@ -61,13 +113,13 @@ def on_progress(stream, chunk, bytes_remaining):
 
 def welcome_message():
     os.system("clear")
-    print(" ██████╗██╗     ██╗██████╗ ██████╗ ██╗████████╗")
-    print("██╔════╝██║     ██║██╔══██╗██╔══██╗██║╚══██╔══╝")
-    print("██║     ██║     ██║██████╔╝██████╔╝██║   ██║   ")
-    print("██║     ██║     ██║██╔═══╝ ██╔══██╗██║   ██║   ")
-    print("╚██████╗███████╗██║██║     ██████╔╝██║   ██║   ")
-    print(" ╚═════╝╚══════╝╚═╝╚═╝     ╚═════╝ ╚═╝   ╚═╝   \n")
-    print("Generate concise meaningful summaries of videos.\n")
+    print("\033[96m ██████╗██╗     ██╗██████╗ ██████╗ ██╗████████╗\033[0m")
+    print("\033[96m██╔════╝██║     ██║██╔══██╗██╔══██╗██║╚══██╔══╝\033[0m")
+    print("\033[96m██║     ██║     ██║██████╔╝██████╔╝██║   ██║   \033[0m")
+    print("\033[96m██║     ██║     ██║██╔═══╝ ██╔══██╗██║   ██║   \033[0m")
+    print("\033[96m╚██████╗███████╗██║██║     ██████╔╝██║   ██║   \033[0m")
+    print("\033[96m ╚═════╝╚══════╝╚═╝╚═╝     ╚═════╝ ╚═╝   ╚═╝   \033[0m")
+    print("\033[94mGenerate concise meaningful summaries of videos.\033[0m\n")
 
 
 if __name__ == "__main__":
--- script.py
+++ script.py
@@ -2,23 +2,31 @@ import os
 import re
 import pysrt
 import logging
+from time import sleep
 from pathlib import Path
 from pytube import YouTube
+from rich.console import Console
 
 logger = logging.getLogger(__name__)
+console = Console()
 
 # Create Captions Object that creates .srt and .txt file of youtube video
 class Captions:
-    def __init__(self, URL):
+    def __init__(self, URL, verbose):
+        # Only prints if verbose flag is set to True
+        self.verboseprint = console.log if verbose else lambda *a, **k: None
+
         self.yt = YouTube(URL)
         self.video_path = os.getcwd() + "/videos/{}".format(
             self.yt.title.lower().replace(" ", "_")
         )
         Path(self.video_path).mkdir(parents=True, exist_ok=True)
+        self.verboseprint("Created new directory for video")
+        sleep(1)
+
         self.srt_filename = self.create_srt()
         self.generate_script()
 
-#
     def create_srt(self):
         """
         This creates the .srt file
@@ -28,6 +36,8 @@ class Captions:
         caption_dict = self.yt.captions.lang_code_index
 
         # Regex searches for keys that start with "en"
+        self.verboseprint("Looking for English Captions")
+        sleep(1)
         en_key = None
         for key in caption_dict.keys():
             if re.findall("^en.*", key):
@@ -35,26 +45,41 @@ class Captions:
                 break
         try:
             if en_key:
+                self.verboseprint("[bold green]Found English subtitles")
                 self.yt.captions[en_key].download("captions")
             elif caption_dict.get("a.en", None):
+                self.verboseprint("[bold green]Found auto-generated captions")
                 self.yt.captions["a.en"].download("captions")
                 en_key = "a.en"
             else:
-                logger.error("clipbit: error in downloading caption file")
-
+                logger.error(
+                    "\033[91m\u2717 clipbit: error in retrieving caption file\033[0m"
+                )
+            sleep(1)
 
             os.rename(f"captions ({en_key}).srt", f"{self.video_path}/captions.srt")
+            self.verboseprint("[bold green]Downloaded script.txt")
+            sleep(1)
+
             return f"{self.video_path}/captions.srt"
 
-        except Exception as e:
-            logger.error(e)
-            logger.error('clipbit: No captions associated with this video.')
+        except Exception:
+            logger.error(
+                "\033[91m\u2717 clipbit: No captions associated with this video.\033[0m"
+            )
 
     def generate_script(self):
-        srt_file = pysrt.open(self.srt_filename)
-        text = ""
-        for obj in srt_file:
-            text += f"{obj.text} "
+        if self.srt_filename:
+            srt_file = pysrt.open(self.srt_filename)
+            text = ""
+            for obj in srt_file:
+                text += f"{obj.text} "
 
-        with open(f"{self.video_path}/script.txt", "w+") as f:
-            f.write(text)
+            with open(f"{self.video_path}/script.txt", "w+") as f:
+                f.write(text)
+            self.verboseprint("[bold green]Created captions.srt file")
+        else:
+            self.verboseprint(
+                "[bold yellow]No .srt file created. Captions most likely not available"
+            )
+        sleep(1)
